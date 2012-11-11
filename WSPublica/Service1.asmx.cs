670819d450572d594ca659184da11e11b32864cc
Roberto Santos <betoroyal10@hotmail.com>
2012-11-11 10:54:52 -0300

    first commit

---
+++ WSPublica/Service1.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+    
+namespace WSPublica
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public string Echo(string s)
+        {
+            return s;
+        }
+
+
+    }
+}
